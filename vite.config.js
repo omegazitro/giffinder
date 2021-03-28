31d6f341205d2fb47ea85efef70ce8a65a439e86
omegazitro <morti137@stu.southtexascollege.edu>
2021-03-28 18:51:41 -0500

    GIF Finder

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import vue from '@vitejs/plugin-vue'
+import ViteComponents from 'vite-plugin-components'
+
+/**
+ * https://vitejs.dev/config/
+ * @type {import('vite').UserConfig}
+ */
+export default {
+  plugins: [vue(), ViteComponents()]
+}
